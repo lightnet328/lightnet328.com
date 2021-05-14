8c5d361e798733f29dc0904133cc82eaf6d74d78
lightnet328 <lightnet328@gmail.com>
2021-05-15 01:30:29 +0900

    Add watch option

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,8 +1,24 @@
-import React from "react";
-import ReactDOMServer from "react-dom/server";
 import fs from "fs";
 import path from "path";
+import arg from "arg";
+import chokidar from "chokidar";
 
 import { render } from "../src/document";
 
-fs.writeFileSync(path.resolve(__dirname, "../index.html"), render());
+const args = arg({
+  "--watch": Boolean,
+});
+
+const build = () => {
+  const filePath = path.resolve(__dirname, "../index.html");
+  fs.writeFileSync(filePath, render());
+  console.log(`${filePath} was generated`);
+};
+
+build();
+
+if (args["--watch"]) {
+  chokidar
+    .watch(path.resolve(__dirname, "../src"), { ignoreInitial: true })
+    .on("all", build);
+}
